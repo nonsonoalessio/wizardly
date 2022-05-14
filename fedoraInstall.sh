cacf16db7fa6dd13c2b86358403bd6691f7b40a5
nonsonoalessio <35069388+nonsonoalessio@users.noreply.github.com>
2022-05-14 20:21:42 +0200

    Update fedoraInstall.sh

--- fedoraInstall.sh
+++ fedoraInstall.sh
@@ -1,6 +1,6 @@
 sudo dnf upgrade -y
 # curl da installare nel primo link
-sudo dnf install git tlp tlp-rdw snapd -y
+sudo dnf install git tlp tlp-rdw snapd zsh -y
 sudo dnf remove power-profiles-daemon -y
 sudo systemctl enable tlp.service
 sudo systemctl mask systemd-rfkill.service systemd-rfkill.socket
@@ -8,16 +8,16 @@ sudo ln -s /var/lib/snapd/snap /snap
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo snap install hello-world
 sudo snap install discord
-sudo flatpak install flathub org.telegram.desktop
+sudo flatpak install flathub org.telegram.desktop -y
 mkdir temp
 cd temp
 sudo curl https://packages.microsoft.com/yumrepos/edge/microsoft-edge-stable-101.0.1210.47-1.x86_64.rpm -o edge.rpm
 sudo curl https://az764295.vo.msecnd.net/stable/da15b6fd3ef856477bf6f4fb29ba1b7af717770d/code-1.67.1-1651841920.el7.x86_64.rpm -o vscode.rpm
-sudo dnf install vscode.rpm
-sudo dnf install edge.rpm
+sudo dnf install vscode.rpm -y
+sudo dnf install edge.rpm -y
 cd
 sudo rm -rf temp
-sudo rm libreoffice*
+sudo rm libreoffice* -y
 sudo snap install android-studio --classic
 sudo snap install flutter --classic
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
