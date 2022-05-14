96ab9da9e8bf4115c0facf2c63a9b28fa164bb82
nonsonoalessio <alessio.giura@gmail.com>
2022-05-14 21:42:56 +0200

    Fine edit Fedora.

--- fedoraInstall.sh
+++ fedoraInstall.sh
@@ -1,5 +1,7 @@
 sudo dnf upgrade -y
-sudo dnf install gh git tlp tlp-rdw snapd zsh -y
+sudo dnf install gh git tlp tlp-rdw snapd zsh python3-pip java-latest-openjdk.x86_64 java-11-openjdk.x86_64 -y
+sudo pip install virtualenv
+curl http://courses.missouristate.edu/kenvollmar/mars/MARS_4_5_Aug2014/Mars4_5.jar -o Mars4_5.jar
 sudo dnf remove power-profiles-daemon -y
 sudo systemctl enable tlp.service
 sudo systemctl mask systemd-rfkill.service systemd-rfkill.socket
@@ -21,9 +23,9 @@ sudo rm -rf temp
 sudo dnf remove libreoffice* -y
 sudo snap install android-studio --classic
 sudo snap install flutter --classic
+curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sudo bash
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 sudo rm .zshrc
 curl https://raw.githubusercontent.com/nonsonoalessio/wizardly/master/res/.zshrc -o .zshrc
 curl https://raw.githubusercontent.com/nonsonoalessio/wizardly/master/res/.p10k.zsh -o .p10k.zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
